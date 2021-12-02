4534822dd845304c0aa480a2cc695ca09f0783b7
Umar Aqdas <92379892+Umar-1309@users.noreply.github.com>
2021-12-02 22:22:27 +0500

    Add files via upload

---
+++ check what is.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	
+	int num[]={10,20,30};
+	cout<<num[0]<<endl;
+	cout<<num<<endl;
+	cout<<*num<<endl;
+	cout<<*(num+ 2);
+	return 0;
+}
+
---
+++ try.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=15;
+    cout<<a;
+    cout<<"Test";
+    cout<<"How are you"<<endl;
+    return 0;
+
+}
